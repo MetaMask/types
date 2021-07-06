2da889412035a3a2173ff527f185189cd238b926
Mark Stacey <markjstacey@gmail.com>
2021-07-06 16:30:15 -0230

    Add `Json` type (#1)
    This type represents any value that can be directly represented as
    JSON. We use this type already in `@metamask/controllers`,
    `json-rpc-engine`, and in a few other places as well.
    
    Tests have been added using `tsd` to verify that the type works
    correctly. The tests are split into sections, where the first section
    verifies that it's compatible with valid JSON data, and the second
    verifies that it's not compatible with invalid JSON. There is a third
    section that identifies known edge cases where the type doesn't behave
    correctly.
    
    `jest` was removed, since there is no code here to test using Jest.
    Similarly, the build was adjusted to just check the type definitions
    without emitting anything.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,21 +7,17 @@ module.exports = {
     {
       files: ['*.ts'],
       extends: ['@metamask/eslint-config-typescript'],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': 'off',
+      },
     },
-
     {
       files: ['*.js'],
-      parserOptions: {
-        sourceType: 'script',
+      env: {
+        commonjs: true,
       },
-      extends: ['@metamask/eslint-config-nodejs'],
-    },
-
-    {
-      files: ['*.test.ts', '*.test.js'],
-      extends: ['@metamask/eslint-config-jest'],
     },
   ],
 
-  ignorePatterns: ['!.eslintrc.js', '!.prettierrc.js', 'dist/'],
+  ignorePatterns: ['!.eslintrc.js', '!.prettierrc.js'],
 };
